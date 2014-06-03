556629bce4d6d994b8f300c1521eb23772dc8ab8
Siddarth Iyer <mindstormer619@gmail.com>
2014-06-03 21:45:55 +0400

    Commit 7: Sum of array elements
    Sum up array elements

---
+++ sum_array.cpp
@@ -0,0 +1,28 @@
+#include <iostream>
+#include <conio.h>
+
+using namespace std;
+
+int sum (int array[], int size) {
+	//sum array = last elem + sum (array till second-last elem)
+	if (size == 1) {
+		return array[size-1]; //first element, actually
+	}
+	return array[size-1] + sum(array, size-1);
+}
+
+int main() {
+	int n; int a[100];
+	cout<<"Enter size of array, <100 :\n";
+	cin>>n;
+	if(n<1 || n>100) {
+		cout<<"Y u do dis?\n";
+		return 1;
+	}
+	for (int i = 0; i<n; i++) {
+		cout<<"Enter next element: ";
+		cin>>a[i];
+	}
+	cout<<"Required sum is "<<sum(a,n);
+	return 0;
+}
