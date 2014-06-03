86228dcad53258628c5c7f8050b71d61841e8159
Siddarth Iyer <mindstormer619@gmail.com>
2014-06-03 19:32:39 +0400

    Commit 3: Descending Numbers
    natural numbers descending

---
+++ desc_num.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+#include <conio.h>
+
+using namespace std;
+
+void descending_natural(int n) {
+	if (1==n) {
+		cout<<n<<" ";
+		return;
+	}
+	cout<<n<<" ";
+	descending_natural(n-1);
+}
+
+int main() {
+	cout<<"Descending numbers from n to some 1, you give n!\n";
+	int n;
+	cin>>n;
+	if (n<1) {
+		cout<<"You, sir, are heartless!\n";
+		return 1;
+	}
+	descending_natural(n);
+	return 0;
+}
